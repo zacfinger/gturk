d331535fabdab143d3413e11a709583134e721f4
Zac Finger <zac@Zacs-MacBook-Pro.local>
2019-03-30 11:34:09 -0700

    Code now sends email using smtplib and email MIME

--- get_results.py
+++ get_results.py
@@ -1,3 +1,5 @@
+# https://blog.mturk.com/tutorial-a-beginners-guide-to-crowdsourcing-ml-training-data-with-python-and-mturk-d8df4bdf2977
+
 # You will need the following library
 # to help parse the XML answers supplied from MTurk
 # Install it in your local environment with
@@ -6,6 +8,9 @@ import xmltodict
 import boto3
 import sqlite3
 import config
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 mturk = boto3.client('mturk',
    aws_access_key_id = config.aws_access_key_id,
@@ -19,22 +24,50 @@ c = conn.cursor()
 #c.execute('SELECT * FROM hits')
 
 for row in c.execute('SELECT * FROM hits'):
-	hit_id = row[2]
-	worker_results = mturk.list_assignments_for_hit(HITId=hit_id)
-	if worker_results['NumResults'] > 0:
-		for assignment in worker_results['Assignments']:
-			xml_doc = xmltodict.parse(assignment['Answer'])
-			turk_email = xml_doc['QuestionFormAnswers']['Answer']['FreeText']
-			# send email here
-			print "updating this shit"
-			c.execute("UPDATE hits SET turk_email=? WHERE hit_id=?", (turk_email, hit_id))
-			conn.commit()
+	# if turk not yet provided email address
+	if row[3] == 0:
+		# check to see if maybe the turk did provide email
+		hit_id = row[2]
+		print hit_id
+		worker_results = mturk.list_assignments_for_hit(HITId=hit_id)
+		print worker_results
+		if worker_results['NumResults'] > 0:
 
-#conn.commit()
-print "updated"
-#cur = conn.cursor()
-#cur.execute(sql, task)
-#some anser
+			print worker_results
+			me = row[0]
+			my_password = row[1]
 
-# We are only publishing this task to one Worker
-# So we will get back an array with one item if it has been completed
+			for assignment in worker_results['Assignments']:
+				xml_doc = xmltodict.parse(assignment['Answer'])
+				turk_email = xml_doc['QuestionFormAnswers']['Answer']['FreeText']
+				# send email here
+
+				msg = MIMEMultipart('alternative')
+				msg['Subject'] = "Generated subject line"
+				msg['From'] = me
+				msg['To'] = turk_email
+
+				html = 'Generated email message'
+				part2 = MIMEText(html, 'html')
+
+				msg.attach(part2)
+
+				# Send the message via gmail's regular server, over SSL - passwords are being sent, afterall
+				s = smtplib.SMTP_SSL('smtp.gmail.com')
+				# uncomment if interested in the actual smtp conversation
+				# s.set_debuglevel(1)
+				# do the smtp auth; sends ehlo if it hasn't been sent already
+				s.login(me, my_password)
+
+				s.sendmail(me, turk_email, msg.as_string())
+				s.quit()
+
+				c.execute("UPDATE hits SET turk_email=?, status=1 WHERE hit_id=?", (turk_email, hit_id))
+				#c.execute("UPDATE hits SET turk_email=?, status=1 WHERE hit_id=?", (turk_email, hit_id))
+				conn.commit()
+	# if turk provided address check to see if email received
+	if row[3] == 1:
+		print row
+		# check to see if email received
+		# and update HITID to 'accepted'
+		# and remove from DB
---
+++ test.py
@@ -0,0 +1,32 @@
+# https://realpython.com/python-send-email/#option-1-setting-up-a-gmail-account-for-development
+# https://stackoverflow.com/questions/53943240/how-to-solve-attributeerror-smtp-ssl-instance-has-no-attribute-exit-in-py
+# https://stackoverflow.com/questions/17759860/python-2-smtpserverdisconnected-connection-unexpectedly-closed
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import config
+
+me = config.sender_email
+my_password = config.password
+you = config.receiver_email
+
+msg = MIMEMultipart('alternative')
+msg['Subject'] = "Alert"
+msg['From'] = me
+msg['To'] = you
+
+html = '<html><body><p>Hi, I have the following alerts for you!</p></body></html>'
+part2 = MIMEText(html, 'html')
+
+msg.attach(part2)
+
+# Send the message via gmail's regular server, over SSL - passwords are being sent, afterall
+s = smtplib.SMTP_SSL('smtp.gmail.com')
+# uncomment if interested in the actual smtp conversation
+# s.set_debuglevel(1)
+# do the smtp auth; sends ehlo if it hasn't been sent already
+s.login(me, my_password)
+
+s.sendmail(me, you, msg.as_string())
+s.quit()
