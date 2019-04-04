1470e948e8ddc75dbf72d5b2ea558f5ed55c2067
Zac Finger <zac@Zacs-MacBook-Pro.local>
2019-04-03 22:50:51 -0700

    Beta-ish version of V2 implementation. looks to be working in tests. need to deploy on server and work out some of the edge cases

--- application.py
+++ application.py
@@ -1,11 +1,8 @@
-# https://www.codementor.io/manojpandey/building-your-first-web-app-using-python-flask-554itecnr
-# https://docs.python.org/3/library/sqlite3.html
-# https://stackoverflow.com/questions/48218065/programmingerror-sqlite-objects-created-in-a-thread-can-only-be-used-in-that-sa
-
 from flask import Flask, render_template, request
 import sqlite3
 import boto3
 import config
+import random, string
 
 app = Flask(__name__)
 
@@ -18,11 +15,18 @@ mturk = boto3.client('mturk',
    endpoint_url = MTURK_SANDBOX
 )
 
-# create table hits (convertist_email TEXT NOT NULL, convertist_email_pw TEXT NOT NULL, hit_id TEXT NOT NULL, status INTEGER NOT NULL, turk_email TEXT NOT NULL, assignment_id TEXT NOT NULL);
+#create table hits (convertist_email TEXT NOT NULL, convertist_email_pw TEXT NOT NULL, hit_id TEXT NOT NULL, status INTEGER NOT NULL, unique_code_1 TEXT NOT NULL, unique_code_2 TEXT NOT NULL, assignment_id TEXT NOT NULL, last_email_checked INTEGER NOT NULL);
+
+def randomCode():
+	# https://stackoverflow.com/questions/2511222/efficiently-generate-a-16-character-alphanumeric-string
+	x = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+	return x
 
 @app.route('/', methods=["GET", "POST"])
 def main():
 	if request.method == "POST":
+
+		random_code = randomCode()
 		
 		convertist_email = request.form.get("convertist_email")
 		convertist_email_pw = request.form.get("convertist_email_pw")
@@ -30,8 +34,8 @@ def main():
 		question = open(name='questions.xml',mode='r').read()
 
 		new_hit = mturk.create_hit(
-		    Title = 'Reply to an email from ' + convertist_email + '',
-		    Description = 'Provide us your email address and respond to the message from ' + convertist_email + '',
+		    Title = 'Write an email to ' + convertist_email + ' and include the code ' + random_code + ' in the subject line',
+		    Description = 'Write an email to ' + convertist_email + ' and include the code ' + random_code + ' in the subject line',
 		    Keywords = 'text, quick, writing',
 		    Reward = '0.15',
 		    MaxAssignments = 1,
@@ -44,11 +48,10 @@ def main():
 		# Insert a row of data
 		conn = sqlite3.connect('database')
 		c = conn.cursor()
-		#c.execute("INSERT INTO hits VALUES ('test@email.com','mural101','AA31337',0,'turk@gmail.com')")
-		
-		# Do this instead
-		t = (convertist_email,convertist_email_pw,new_hit['HIT']['HITId'],0,'','')
-		c.execute("INSERT INTO hits VALUES (?,?,?,?,?,?)", t)
+		t = (convertist_email,convertist_email_pw,new_hit['HIT']['HITId'],0,random_code,'','',0)
+		#convertist_email , convertist_email_pw, hit_id, status, unique_code_1, unique_code_2, assignment_id, last_email_checked
+
+		c.execute("INSERT INTO hits VALUES (?,?,?,?,?,?,?,?)", t)
 
 		conn.commit()
 		#print(c.fetchone())
--- get_results.py
+++ get_results.py
@@ -1,10 +1,19 @@
+# GTURK
+# Mturk + Gmail = Gturk
+# 1024 times better than Mturk
+# (C)2019 Zacfinger.com
+
+# TODO
+# if no turk has responded to the HIT 
+# fix async events when SQLite db updated
+# search in body rather than subject line
+
+
+# Following resources were helpful
 # https://blog.mturk.com/tutorial-a-beginners-guide-to-crowdsourcing-ml-training-data-with-python-and-mturk-d8df4bdf2977
 # https://stackoverflow.com/questions/16856647/sqlite3-programmingerror-incorrect-number-of-bindings-supplied-the-current-sta
+# https://codehandbook.org/how-to-read-email-from-gmail-using-python/
 
-# You will need the following library
-# to help parse the XML answers supplied from MTurk
-# Install it in your local environment with
-# pip install xmltodict
 import xmltodict
 import boto3
 import sqlite3
@@ -23,6 +32,9 @@ mturk = boto3.client('mturk',
    endpoint_url = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
 )
 
+def newCode(input_string):
+	return input_string
+
 conn = sqlite3.connect('database')
 c = conn.cursor()
 #c.execute('SELECT * FROM hits')
@@ -31,90 +43,125 @@ for row in c.execute('SELECT * FROM hits'):
 	me = row[0]
 	my_password = row[1]
 
-	# if turk not yet provided email address
+	# if status is 0
+		# open inbox of convertist_email to see if someone sent message with code in subject line
+		# for each message read and unread
+			# if code sent in subject line matches the code in row
+				# send response back to user with a corresponding code (use some kind of cipher)
+				# set status on db to 1 and save code_2 in db
+			# else mark as read/unread whatever value it was before
+	# if status is 1 (meaning we have sent back email to user)
+		# for each assignment ID in corresponding HIT ID
+			# check to see if response from user equals code sent
+				# if code_2 cipher code_1 works mark assignment as complete and delete from db
+			# else if response is wrong reject assignment and delete from db
+
+	# if email has been created, HIT task created, but no response yet from any user
 	if row[3] == 0:
-		# check to see if turk provided email address
-		hit_id = row[2]
-		print hit_id
-		worker_results = mturk.list_assignments_for_hit(HITId=hit_id)
-		print worker_results
-		if worker_results['NumResults'] > 0:
-
-			count = 0
-
-			for assignment in worker_results['Assignments']:
-				assignment_id = assignment['AssignmentId']
-				xml_doc = xmltodict.parse(assignment['Answer'])
-				turk_email = xml_doc['QuestionFormAnswers']['Answer']['FreeText']
-				# send email here
-
-				msg = MIMEMultipart('alternative')
-				msg['Subject'] = "Generated subject line"
-				msg['From'] = me
-				msg['To'] = turk_email
-
-				html = 'Generated email message'
-				part2 = MIMEText(html, 'html')
-
-				msg.attach(part2)
-
-				# Send the message via gmail's regular server, over SSL - passwords are being sent, afterall
-				s = smtplib.SMTP_SSL('smtp.gmail.com')
-				# uncomment if interested in the actual smtp conversation
-				# s.set_debuglevel(1)
-				# do the smtp auth; sends ehlo if it hasn't been sent already
-				s.login(me, my_password)
-
-				s.sendmail(me, turk_email, msg.as_string())
-				s.quit()
-
-				if count == 0:
-
-					c.execute("UPDATE hits SET turk_email=?, status=1, assignment_id=? WHERE hit_id=?", (turk_email, assignment_id, hit_id))
-					#c.execute("UPDATE hits SET turk_email=?, status=1 WHERE hit_id=?", (turk_email, hit_id))
-					conn.commit()
-
-					count = count + 1
-				else:
-					c.execute("INSERT INTO hits(convertist_email, convertist_email_pw, hit_id, status, turk_email, assignment_id) VALUES (?,?,?,?,?,?)", (me, my_password, hit_id, 1, turk_email, assignment_id))
-					conn.commit()
-	# if turk provided address check to see if email received
-	if row[3] == 1:
+		# open inbox of convertist_email to see if someone sent message with code in subject line
 		SMTP_SERVER = "imap.gmail.com"
-		print "got into thhis block"
+		print "got into this block"
 		try:
 			mail = imaplib.IMAP4_SSL(SMTP_SERVER)
 			mail.login(me,my_password)
-			mail.select('inbox')
+			mail.select('[Gmail]/All Mail')
 
 			type, data = mail.search(None, 'ALL')
 			mail_ids = data[0]
 
 			id_list = mail_ids.split()
-			first_email_id = int(id_list[0])
 			latest_email_id = int(id_list[-1])
 
-			for i in range(latest_email_id,first_email_id, -1):
-				typ, data = mail.fetch(i, '(RFC822)' )
+			if row[7] == 0:
+				first_email_id = int(id_list[0])	
+			else:
+				first_email_id = row[7]
 
-				for response_part in data:
-					if isinstance(response_part, tuple):
-						msg = email.message_from_string(response_part[1])
-						email_subject = msg['subject']
-						email_from = msg['from']
-						first_char = email_from.find('<') + 1
-						last_char = email_from.find('>')
-						email_from = email_from[first_char:last_char]
-						if email_from == row[4]:
-							print "approve via mechanical turk API"
-							mturk.approve_assignment(AssignmentId=row[5])
-							c.execute("DELETE from hits WHERE assignment_id=?", (row[5],))
-							conn.commit()
+			email_sent = 0
+
+			# for each message read and unread
+			for i in range(latest_email_id,first_email_id, -1):
+				print i
+				if email_sent == 0:
+					typ, data = mail.fetch(i, '(RFC822)' )
+					for response_part in data:
+						if isinstance(response_part, tuple) and email_sent == 0:
+							msg = email.message_from_string(response_part[1])
+							email_subject = msg['subject']
+							if email_subject is not None:
+								words = email_subject.split(" ")
+
+							for word in words:
+								# if code sent in subject line matches the code in row
+								# get_results.py:88: UnicodeWarning: Unicode equal comparison failed to convert both arguments to Unicode - interpreting them as being unequal
+								if word == row[4]:
+									print "it worked!"
+									new_code = newCode(row[4])
+									email_from = msg['from']
+									first_char = email_from.find('<') + 1
+									last_char = email_from.find('>')
+									email_from = email_from[first_char:last_char]
+									
+									# send response back to user with a corresponding code (use some kind of cipher)
+									msg = MIMEMultipart('alternative')
+									msg['Subject'] = "Generated subject line" + new_code
+									msg['From'] = me
+									msg['To'] = email_from
+
+									html = 'Generated email message'
+									part2 = MIMEText(html, 'html')
+
+									msg.attach(part2)
+
+									# Send the message via gmail's regular server, over SSL - passwords are being sent, afterall
+									s = smtplib.SMTP_SSL('smtp.gmail.com')
+									
+									# do the smtp auth; sends ehlo if it hasn't been sent already
+									s.login(me, my_password)
+
+									s.sendmail(me, email_from, msg.as_string())
+									s.quit()
+
+									# set status on db to 1 and save code_2 in db
+									c.execute("UPDATE hits SET status=1, unique_code_2=?, last_email_checked=? WHERE unique_code_1=?", (new_code,i,word))
+									#convertist_email, convertist_email_pw, hit_id, status, unique_code_1, unique_code_2, assignment_id, last_email_checked
+									conn.commit()
+									email_sent = 1
+									break
+
+						else:
+							break
+				else:
+					break
+
+			if email_sent == 0:
+				c.execute("UPDATE hits SET last_email_checked=? WHERE unique_code_1=?", (latest_email_id,row[4]))
+				conn.commit()
 
 		except Exception, e:
 			print str(e)
-			# check to see if email received
-			# and update HITID to 'accepted'
-			# and remove from DB
+	# if status is 1 (meaning we have sent back email to user)
+	if row[3] == 1:
+		print "status is 1"
+		hit_id = row[2]
+		worker_results = mturk.list_assignments_for_hit(HITId=hit_id)
+		print worker_results
+		if worker_results['NumResults'] > 0:
+			# for each assignment ID in corresponding HIT ID
+			for assignment in worker_results['Assignments']:
+				assignment_id = assignment['AssignmentId']
+				xml_doc = xmltodict.parse(assignment['Answer'])
+				turk_response = xml_doc['QuestionFormAnswers']['Answer']['FreeText']
+				# check to see if response from user equals code sent
+				if turk_response == row[5]:
+					# if code_2 cipher code_1 works mark assignment as complete and delete from db
+					mturk.approve_assignment(AssignmentId=assignment_id)
+					c.execute("DELETE from hits WHERE unique_code_2=?", (turk_response,))
+					conn.commit()
+				else:
+					mturk.reject_assignment(AssignmentId=assignment_id,RequesterFeedback="Incorrect code provided")
+					c.execute("DELETE from hits WHERE unique_code_1=?", (row[4],))
+		else:
+			print "no responses currently"
 
 print "finished"
